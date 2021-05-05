99ab5655fb4d2d30af12b210aee74198df51dca5
Wes Higbee <wes.mcclure@gmail.com>
2021-05-05 15:12:07 -0400

    remove all contexts by starting letters, perhaps i can use ./vagrant/machines later to be more precise albiet that could falter too, vagrant status might be best but even that might be missing removed vms

--- labs/vagrants/host/host.sh
+++ labs/vagrants/host/host.sh
@@ -7,9 +7,11 @@ echo '  running: '${running_vms_space_delimited}
 
 vagrant ssh-config ${running_vms_space_delimited} > ~/.ssh/config.d/vagrant_swarmgs
 
-echo '2. adding docker ssh contexts to vms'
+echo '2. remove existing contexts, thus nuking dangling contexts (vms not running) and can just assume create instead of conditional update'
+docker context rm $(docker context list -q | grep '^[wm]')
+echo 
+echo '3. adding docker ssh contexts to vms'
 for vm in $running_vms; do 
-  docker context rm $vm 
   docker context create \
     --docker "host=ssh://$vm" \
     --default-stack-orchestrator swarm \
