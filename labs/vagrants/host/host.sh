6898fade80cd1e8448a34f8be8d40bf918231f68
Wes Higbee <wes.mcclure@gmail.com>
2021-05-05 21:25:25 -0400

    update messages

--- labs/vagrants/host/host.sh
+++ labs/vagrants/host/host.sh
@@ -5,15 +5,19 @@ running_vms=$(vagrant status --machine-readable | grep 'state,running' | cut -d'
 running_vms_space_delimited=${running_vms[@]}
 echo '  running: '${running_vms_space_delimited}
 
+echo '2. write vagrant ssh config options for faster, native/direct ssh access to running vms'
 vagrant ssh-config ${running_vms_space_delimited} > ~/.ssh/config.d/vagrant_swarmgs
+echo
 
-echo '2. remove existing contexts, thus nuking dangling contexts (vms not running) and can just assume create instead of conditional update'
+echo '3. remove existing contexts starting with `w` or `m`'
 docker context rm $(docker context list -q | grep '^[wm]')
 echo 
-echo '3. adding docker ssh contexts to vms'
+
+echo '4. adding docker ssh contexts for running vms'
 for vm in $running_vms; do 
   docker context create \
     --docker "host=ssh://$vm" \
     --default-stack-orchestrator swarm \
     $vm
 done
+echo
