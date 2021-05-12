6e3f9f4a7f7cc0fd15a49669cb078463d87aa58b
Wes Higbee <wes.mcclure@gmail.com>
2021-05-12 15:27:13 -0400

    add viz.sh for service create alternative

---
+++ stacks/viz.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+## Create a service by hand
+
+docker service create \
+  --name=viz_dsc \
+  --publish=8080:8080/tcp \
+  --constraint=node.role==manager \
+  --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+  dockersamples/visualizer
