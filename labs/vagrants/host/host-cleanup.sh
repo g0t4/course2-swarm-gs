32ed8147ac98c2622cec886209668c13eeb22a12
Wes Higbee <wes.mcclure@gmail.com>
2021-05-14 11:50:21 -0400

    messaging

--- labs/vagrants/host/host-cleanup.sh
+++ labs/vagrants/host/host-cleanup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+echo "## removing native ssh config to VMs"
 rm ~/.ssh/config.d/vagrant_swarmgs 
+echo
 
+echo "## finding all VM names"
 _vms=( $(vagrant status --machine-readable | cut -d',' -f2 | sort | uniq) )
+echo
 
+echo "## removing all docker contexts for VMs"
 docker context rm -f "${_vms[@]}"
 
--- labs/vagrants/host/host-setup.sh
+++ labs/vagrants/host/host-setup.sh
@@ -2,15 +2,18 @@
 
 echo '## find running VMs'
 running_vms=( $(vagrant status --machine-readable | grep 'state,running' | cut -d',' -f2 | sort | uniq) )
-
 running_vms_spaced=${running_vms[@]}
+echo 
 
 echo '## config native ssh access via `vagrant ssh-config`'
-echo '## b/c faster & docker ssh context needs it'
+echo '##   native is faster'
+echo '##   necessary for docker ssh context'
 vagrant ssh-config $running_vms_spaced > ~/.ssh/config.d/vagrant_swarmgs
+echo
 
-echo '## ignore already exists warning, this is idempotent'
-echo '## or just run host-cleanup.sh first'
+echo '## creating docker contexts to running vms'
+echo '## * ignore already exists warning *'
+echo '##   or run host-cleanup.sh first'
 for vm in $running_vms_spaced; do
   docker context create \
     --docker "host=ssh://$vm" \
