4ab2727da0f51f57c0c5f3295a5c5ee1ae9c90fb
Wes Higbee <wes.mcclure@gmail.com>
2021-05-17 02:45:05 -0400

    throw together a quick sh script with just a call to docker container run to launch manomarks (dockersamples/visualizer) as a container via standalone docker engine apis to compare and contrast next with a service and for now to get pretty visuals of nodes on the cluster!

---
+++ examples/viz/viz-container.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+docker -c m1 \
+  container run -d \
+  -p "8080:8080/tcp" \
+  -v "/var/run/docker.sock:/var/run/docker.sock" \
+  dockersamples/visualizer
+
+# Notes
+# - I am manually placing this on a manager node
+# - its a standalone container so side by side with swarm objects/resources
+# - this will remain familiar
+# - to locate UI I can only go to m1 (will this remain the same with the cluster? ;) )
