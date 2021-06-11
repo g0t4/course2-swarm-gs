96944828c05d312e19445389805abc121c88ce0b
Wes Higbee <wes.mcclure@gmail.com>
2021-06-11 09:57:22 -0400

    ARRANGE - move over a few observation scripts into services folder

--- examples/services/observe/watch-services.sh
+++ examples/services/observe/watch-services.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
 
+# watches should be run in VM for performance 
+#   or slow down frequency of checking (ie -n 2)
 _cmd="docker service ls; echo; echo; docker service ps $1"
 watch -n0.5 -d $_cmd
