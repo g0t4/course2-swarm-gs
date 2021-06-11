2968004e20f29ea40c3b5c7fb18a40646831ecbe
Wes Higbee <wes.mcclure@gmail.com>
2021-06-11 09:35:51 -0400

    ADDITIONS - Add some additional commands both shown and not shown

--- examples/services/convert-viz-to-service.sh
+++ examples/services/convert-viz-to-service.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-## Create a service by hand
+# First, we created viz as a standalone container, a great candidate for a first service!
+
+## 1. Create a service "by hand"
+# Note: something is missing intentionally
 
 docker service create \
   --name=viz \
@@ -8,21 +11,28 @@ docker service create \
   --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
   dockersamples/visualizer
 
+
+## 2. Updating a service "by hand"
+
 docker service update \
   --constraint-add node.role==manager \
   viz
 
 
-
-
-
-
-
-
-## commands:
+## Watching for task changes to viz
 
 # watches should be run in VM for performance 
-# or slow down frequency of checking (ie -n 2)
+#   or slow down frequency of checking (ie -n 2)
 watch -t -d -n 0.5 'docker service ps --format "table {{.ID}}\t{{.Name}}\t{{.Node}}\t{{.DesiredState}}\t{{.CurrentState}}" viz'
+# add/rm columns based on scenario
+# here are the ones I left out: 
+#   \t{{.Image}}\t{{.Error}}\t{{.Ports}}
+# see all fields: 
+#   --format "table"
+# use json to find casing/spelling of fields:
+#   --format "{{json .}}"
+
+
+## viz Container logs
 
 docker service logs viz 
--- examples/services/nginx-service.sh
+++ examples/services/nginx-service.sh
@@ -1,3 +1,5 @@
+## Create nginx service
+
 docker service create \
     --publish "8200:80/tcp" \
     --name weby \
@@ -5,19 +7,26 @@ docker service create \
     nginx
 
 
+# Watch weby task changes
 
-
-
-
-
-
-
-## commands:
-
-# watches should be run in VM for performance 
-# or slow down frequency of checking (ie -n 2)
+# - watches should be run in VM for performance 
+#   - or slow down frequency of checking (ie -n 2)
 watch -t -d -n 0.5 'docker service ps --format "table {{.ID}}\t{{.Name}}\t{{.Node}}\t{{.DesiredState}}\t{{.CurrentState}}" weby'
 
 
+## Remove service
+
+docker service rm weby
+
+
+## Service logs (containers)
 
 docker service logs weby
+
+
+## Scale service
+
+docker service scale weby=6
+# scale is shorthand for `update --replicas` 
+docker service update --replicas=6 weby
+
