6e914e8fca0d606bf3c0afd4fcf827ee66166790
Wes Higbee <wes.mcclure@gmail.com>
2021-06-21 06:27:10 -0400

    wrap up scripts to share

--- examples/stacks/stress/jobs/01-ab1.sh
+++ examples/stacks/stress/jobs/01-ab1.sh
@@ -7,6 +7,3 @@ docker service create \
   --network sr_testers \
   httpd \
   ab -n 100 -c 10 http://sr_web:3000/customer-nodelay/1
-
-# FYI show how much faster this appears to be when run within the cluster! versus just a few switch hops away on my MBP (sub millisecond ping time too, so low latency)
-# perhaps ping would be instrumental though too?
---
+++ examples/stacks/stress/jobs/05-ab-5on5.sh
@@ -0,0 +1,13 @@
+docker service rm ab5on5
+
+docker service update \
+  --replicas-max-per-node 1 \
+  --replicas 5 \
+  sr_web
+
+docker service create \
+  --name ab5on5 \
+  --mode global-job \
+  --network sr_testers \
+  httpd \
+  ab -n 1000 -c 100 http://sr_web:3000/customer-nodelay/1
