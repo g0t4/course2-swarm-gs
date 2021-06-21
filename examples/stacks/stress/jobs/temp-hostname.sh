814f3d7beaecd55c227ecd2a40ff692faa275aca
Wes Higbee <wes.mcclure@gmail.com>
2021-06-21 05:24:23 -0400

    commands for looking at how templating works (is it similar or different!? how would one find out if a job finishes quickly!)

---
+++ examples/stacks/stress/jobs/temp-hostname.sh
@@ -0,0 +1,18 @@
+echo 'understanding how templating (hostname) works with jobs'
+
+set -x 
+
+_service_name='temp-hostname'
+docker service rm "${_service_name}"
+
+docker service create \
+  --name "${_service_name}"  \
+  --mode replicated-job \
+  --hostname "${_service_name}-{{.Node.Hostname}}-{{.Task.Slot}}" \
+  --replicas 5 \
+  httpd \
+  bash -c 'hostname'
+
+docker service logs "${_service_name}"
+
+docker inspect (docker service ps "${_service_name}" -q)
