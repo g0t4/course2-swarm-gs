79bf4b6c4c6dffad778a85d8bfd30707878b88c5
Wes Higbee <wes.mcclure@gmail.com>
2021-06-21 06:02:41 -0400

    more job scripts

--- examples/stacks/stress/jobs/04-ab5x.sh
+++ examples/stacks/stress/jobs/04-ab5x.sh
@@ -2,7 +2,11 @@
 docker service create \
   --name ab1x10 \
   --mode replicated-job \
-  --replicas 1 \
+  --replicas 5 \
   --network sr_testers \
   httpd \
   ab -n 1000 -c 100 http://sr_web:3000/customer-nodelay/1
+
+echo "don't forget to scale the sr_web service as you see fit"
+echo "service currently:"
+docker service ls sr_web
